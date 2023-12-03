e3e595b5bca987ddcef7748c86fa7e7e7a4aedcc
tsimeonov <tsimeonov12@gmail.com>
2023-12-03 15:50:37 +0100

    start coding the functionallity

--- code/app.js
+++ code/app.js
@@ -10,3 +10,23 @@ hamburger.addEventListener("click", () => {
 closeBtn.addEventListener("click", () => {
   sidebar.classList.remove("open");
 });
+
+// FAQ SECTION
+const faqs = document.querySelectorAll(".faq");
+const createPlanDivOne = document.querySelector(
+  ".create-plan-nav > div:nth-of-type(1)"
+);
+
+const faqOne = document.querySelector(
+  ".create-plan-dropdown > div:nth-of-type(1)"
+);
+
+faqs.forEach((faq) => {
+  faq.addEventListener("click", () => {
+    faq.classList.toggle("active");
+  });
+});
+
+createPlanDivOne.addEventListener("click", () => {
+  faqOne.classList.toggle("active");
+});
